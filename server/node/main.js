a85d68a4663735ba9d42298e8f43a240fad57d95
Kenneth Lakin <kennethlakin@gmail.com>
2013-03-27 15:07:20 -0700

    Removing Array.remove.

--- server/node/main.js
+++ server/node/main.js
@@ -1,9 +1,3 @@
-Array.prototype.remove = function(from, to) {
-  var rest = this.slice(parseInt(to || from) + 1 || this.length);
-  this.length = from < 0 ? this.length + from : from;
-  return this.push.apply(this, rest);
-};
-
 /**
  * Our data looks like this:
  * Project:
